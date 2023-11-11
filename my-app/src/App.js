5e6341e864545ba9ff3ee0f2431c1ef2a073626d
pkumars397 <pkumars397@gmail.com>
2023-11-11 14:01:41 +0530

    task done

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+// import "./App.css";
+import ExpenseItems from "./components/ExpenseItems";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets get Started</h1>
+      <ExpenseItems></ExpenseItems>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/ExpenseItems.js
@@ -0,0 +1,12 @@
+function ExpenseItems() {
+  return (
+    <div>
+      <h1>Expense Items</h1>
+      <h2>Food Rs 10</h2>
+      <h2>Petrol Rs 100</h2>
+      <h2>Movies Rs 200</h2>
+    </div>
+  );
+}
+
+export default ExpenseItems;
