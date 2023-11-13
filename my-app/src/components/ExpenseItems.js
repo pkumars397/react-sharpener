85948156969614ff0f19f7276117fe251842decc
pkumars397 <pkumars397@gmail.com>
2023-11-13 11:16:47 +0530

    done usiing variale in jsx

--- my-app/src/components/ExpenseItems.js
+++ my-app/src/components/ExpenseItems.js
@@ -1,10 +1,18 @@
+import "./ExpenseItems.css";
+
 function ExpenseItems() {
+  const expenseDate = new Date(2023, 3, 21);
+  const expenseTitle = "Car Insurance";
+  const expensePrice = 290;
+  const LocationOfExpenditure = "Bangalore";
   return (
-    <div>
-      <h1>Expense Items</h1>
-      <h2>Food Rs 10</h2>
-      <h2>Petrol Rs 100</h2>
-      <h2>Movies Rs 200</h2>
+    <div className="expense-item">
+      <div>{expenseDate.toLocaleDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expensePrice}</div>
+        <h3>{LocationOfExpenditure}</h3>
+      </div>
     </div>
   );
 }
